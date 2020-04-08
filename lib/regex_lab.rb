4837c4d2b1cfbdcd19164b456b039bd8566e17b5
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-08 17:54:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ word.match?(/^[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/^[UNun]\w+ing/)
+ text.match(/^[UNun]\w+ing/)
 end
 
 def words_five_letters_long(text)
